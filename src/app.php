6fe6fc1e0ee6a427d09edda46d0ecbd2f35d6dd7
Ian Monge <ian.monge@socialpoint.es>
2013-05-24 00:21:57 +0200

    Starting to add security to main sections

--- src/app.php
+++ src/app.php
@@ -20,4 +20,17 @@ $app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
     return $twig;
 }));
 
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'admin' => array(
+            'pattern' => '^/admin',
+            'http' => true,
+            'users' => array(
+                // password is foo
+                'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+            ),
+        ),
+    ),
+));
+
 return $app;
